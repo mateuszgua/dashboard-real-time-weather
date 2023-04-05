11f887afdfc6214cab921afa2a7b3abc5d0a8068
Mateusz Gula <mateuszgua@wp.eu>
2023-04-05 13:14:44 +0200

    Feat: change producer and remove unused code

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -11,25 +11,17 @@ class MessageProducer:
         self.broker = broker
         self.topic = topic
         self.producer = KafkaProducer(bootstrap_servers=self.broker,
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-                         acks='all',
-                         retries=3)
-    
+                                      value_serializer=lambda v: json.dumps(
+                                          v).encode('utf-8'),
+                                      acks='all',
+                                      retries=3)
+
     def send_msg(self, msg):
         try:
             future = self.producer.send(self.topic, msg)
             self.producer.flush()
             future.get(timeout=60)
             print("message sent successfully...")
-            return {'status_code': 200, 'error': None }
+            return {'status_code': 200, 'error': None}
         except Exception as e:
             return e
-        
-
-broker = 'localhost:9092'
-topic = 'test-topic'
-message_producer = MessageProducer(broker, topic)
-
-data = {'name':'abc', 'email':'abc@abc.com'}
-resp = message_producer.send_msg(data)
-print(resp)
