e9631dbfe3d39a0adc3ac444f7b6393e4c55aa1e
Mateusz Gula <mateuszgua@wp.eu>
2023-04-05 12:07:09 +0200

    Feat: create kafka broker in localhost and create send message function

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -14,3 +14,22 @@ class MessageProducer:
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'),
                          acks='all',
                          retries=3)
+    
+    def send_msg(self, msg):
+        try:
+            future = self.producer.send(self.topic, msg)
+            self.producer.flush()
+            future.get(timeout=60)
+            print("message sent successfully...")
+            return {'status_code': 200, 'error': None }
+        except Exception as e:
+            return e
+        
+
+broker = 'localhost:9092'
+topic = 'test-topic'
+message_producer = MessageProducer(broker, topic)
+
+data = {'name':'abc', 'email':'abc@abc.com'}
+resp = message_producer.send_msg(data)
+print(resp)
